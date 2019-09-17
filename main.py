aff3c76939e5270132f9273568e93471bd2884d5
dosisod <39638017+dosisod@users.noreply.github.com>
2019-09-16 20:18:36 -0700

    added better args parse, added help message

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import sys
 import os
 
-if __name__=="__main__":
-	args=sys.argv[1:]
+HELP_MSG="""USAGE: python3 main.py [Command] [Params]
+
+(No command)  - Print all tasks
+h[elp]        - This message
+i[nit]        - Make .todo file
+a[dd] STR     - Add STR to todo list
+r[m] NUM      - Removes task NUM from list
+s[earch] STR  - Search and print tasks that match substring STR
+r[egex] REG   - Search and print tasks that match regex REG
+v[im]         - Open list in vim
+v[im] REG     - Open list in vim with regex REG highlighted
+"""
+
+def find():
 	path=os.getcwd().split("/")
 
 	filen=None
@@ -18,4 +30,25 @@ if __name__=="__main__":
 
 		path=path[:-1]
 
-	print(filen)
+	return filen
+
+if __name__=="__main__":
+	args=sys.argv[1:] #remove filename from args
+
+	filen=find()
+
+	if len(args)>1: #merges all args after first arg into one
+		args=[
+			args[0].lower(),
+			" ".join(str(i) for i in args[1:])
+		]
+
+	if len(args)==0: #user just wants to print todo list
+		pass
+
+	elif len(args)==1: #a command is being ran w/o params
+		if args[0]=="help":
+			print(HELP_MSG)
+
+	else: #len(args) is 3 (command with params is being ran)
+		pass
